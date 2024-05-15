a84d53fb336ca308858f53bd0477e5f560712818
bearone236 <kuma_2306@icloud.com>
2024-05-15 11:23:46 +0900

    不必要なファイルの削除

--- src/app/api/otp/route.ts
+++
@@ -1,7 +0,0 @@
-// import { NextResponse } from "next/server";
-// import { generateOtp } from "../../../utils/otp";
-
-// export async function GET() {
-//   const otp = generateOtp();
-//   return NextResponse.json({ otp });
-// }
